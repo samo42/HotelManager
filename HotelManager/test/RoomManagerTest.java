6461b83e06ed7d453bef7d5b740978d06f61d543
samo42 <petovskysamuel@gmail.com>
2014-03-10 21:47:40 +0100

    Room create and update test

--- HotelManager/test/RoomManagerTest.java
+++ HotelManager/test/RoomManagerTest.java
@@ -3,12 +3,11 @@
  * and open the template in the editor.
  */
 
-import Entities.Guest;
 import Entities.Room;
-import org.junit.After;
-import org.junit.AfterClass;
+import Interfaces.RoomManager;
+import hotelmanager.RoomManagerImpl;
+import javax.net.ssl.SSLEngineResult;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -19,18 +18,204 @@ import static org.junit.Assert.*;
 public class RoomManagerTest {
     
     private Room room;
+    private Room roomCapacityNull;
+    private Room roomCapacityZero;
+    private Room roomNumberNull;
+    private Room roomNumberEmpty;
+    private Room roomDescriptionNull;
+    private Room roomDescriptionEmpty;
+    private Room nullRoom;
     
-    public RoomManagerTest() {
-    }
+    private RoomManager roomManager = new RoomManagerImpl();
     
     @Before
     public void setUp() {
         room = new Room();
+        room = createRoom(5, "Luxury room", 200, "001");
+        
+        roomCapacityNull = new Room();
+        roomCapacityNull = createRoom(null, "Small room", 100, "002");
+                
+        roomCapacityZero = new Room();
+        roomCapacityZero = createRoom(0, "Nobody room", 20, "003");
+        
+        roomNumberNull = new Room();
+        roomNumberNull = createRoom(4, "Nowhere room", 600, null);
+                
+        roomNumberEmpty = new Room();
+        roomNumberEmpty = createRoom(3, "Somewhere room", 120, "");
+        
+        roomDescriptionNull = new Room();
+        roomDescriptionNull = createRoom(7, null, 115, "004");
+        
+        roomDescriptionEmpty = new Room();
+        roomDescriptionEmpty = createRoom(1, "", 115, "005");
+        
+        nullRoom = null;
     }
     
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void createRoomTest(){
+        try{
+            roomManager.createRoom(roomCapacityNull);
+            fail("Create test - null capacity no exception");
+        }catch (NullPointerException ex){
+        }catch (Exception ex){
+                fail ("Create test - null capacity bad exception" + ex);
+        }
+        
+        try{
+            roomManager.createRoom(roomCapacityZero);
+            fail("Create test - zero capacity no exception");
+        }catch (IllegalArgumentException ex){
+        }catch (Exception ex){
+                fail ("Create test - zero capacity bad exception" + ex);
+        }
+        
+        try{
+            roomManager.createRoom(roomDescriptionEmpty);
+            fail("Create test - empty description no exception");
+        }catch (IllegalArgumentException ex){
+        }catch (Exception ex){
+                fail ("Create test - empty description bad exception" + ex);
+        }
+        
+        try{
+            roomManager.createRoom(roomDescriptionNull);
+            fail("Create test - null description no exception");
+        }catch (NullPointerException ex){
+        }catch (Exception ex){
+                fail ("Create test - null description bad exception" + ex);
+        }
+        
+        try{
+            roomManager.createRoom(roomNumberEmpty);
+            fail("Create test - empty number no exception");
+        }catch (IllegalArgumentException ex){
+        }catch (Exception ex){
+                fail ("Create test - empty number bad exception" + ex);
+        }
+        
+        try{
+            roomManager.createRoom(roomNumberNull);
+            fail("Create test - null number no exception");
+        }catch (NullPointerException ex){
+        }catch (Exception ex){
+                fail ("Create test - null number bad exception" + ex);
+        }
+        
+        try{
+            roomManager.createRoom(nullRoom);
+            fail("Create test - room is null no exception");
+        }catch (IllegalArgumentException ex){
+        }catch (Exception ex){
+                fail ("Create test - room is null bad exception" + ex);
+        }
+        
+        try{
+            Room returnedRoom = roomManager.createRoom(room);
+            assertDeepEquals(room, returnedRoom);
+        }catch (Exception ex){
+                fail ("Create test - zero capacity bad exception" + ex);
+        }
+        
+    }
+    
+    @Test
+    public void updateRoomImplTest(){
+//        Room roomCapacityNull = createRoom();
+//        roomManager.createRoom(roomCapacityNull);
+//        roomCapacityNull.setCapacity(null);
+        
+        Room room1 = createRoom(1,"Small Room",100,"001");
+        Room room2 = createRoom(2,"Double Room", 180,"002");
+        Room room3 = createRoom(2,"Twin Room", 180, "003");
+        
+        //will be replaced with manual transaction
+        roomManager.createRoom(room1); 
+        roomManager.createRoom(room2);
+        roomManager.deleteRoom(room2);
+        
+        try{
+            room1.setDescription("Updated Room");
+            roomManager.updateRoom(room);
+        }catch (Exception ex){
+                fail ("Update test - bad exception" + ex);
+        }
+        
+        Room equalRoom = roomManager.findRoom(room1.getId());
+        assertDeepEquals(room1, equalRoom);
+        
+        try{
+            room2.setDescription("Updated Room2");
+            roomManager.updateRoom(room2);
+            fail("deleted room - no exception");
+        }catch (IllegalArgumentException ex){
+        }catch (Exception ex){
+            fail("deleted room - bad exception");
+        }
+        
+        try{
+            room3.setDescription("Updated Room3");
+            roomManager.updateRoom(room3);
+            fail("room no ID - no exception");
+        }catch (NullPointerException ex){
+        }catch (Exception ex){
+            fail("room no ID - bad exception");
+        }
+        
+        try{
+            Room roomNull = room1;
+            roomNull.setCapacity(null);
+            roomNull.setDescription(null);
+            roomNull.setPriceForNight(null);
+            roomNull.setRoomNumber(null);
+            roomManager.updateRoom(roomNull);
+            fail("Update test - null arguments");
+        }catch (NullPointerException ex){
+        }catch (Exception ex){
+                fail ("Create test - null capacity bad exception" + ex);
+        }
+        
+        try{
+            Room roomEmpty = room1;
+            roomEmpty.setDescription("");
+            roomEmpty.setRoomNumber("");
+            roomManager.updateRoom(roomEmpty);
+            fail("Update test - empty arguments");
+        }catch (IllegalArgumentException ex){
+        }catch (Exception ex){
+                fail ("Update test - empty arguments bad exception" + ex);
+        }
+        
+        try{
+            Room roomZeroCapacity = room1;
+            roomZeroCapacity.setCapacity(0);
+            roomManager.updateRoom(roomZeroCapacity);
+            fail("Update test - zero capacity");
+        }catch (IllegalArgumentException ex){
+        }catch (Exception ex){
+                fail ("Create test - zero capacity bad exception" + ex);
+        }
+        
+    }
+    
+    
+    private void assertDeepEquals(Room room1, Room room2){
+        assertEquals(room1.getCapacity(), room2.getCapacity());
+        assertEquals(room1.getDescription(), room2.getDescription());
+        assertEquals(room1.getPriceForNight(), room2.getPriceForNight());
+        assertEquals(room1.getRoomNumber(), room2.getRoomNumber());
+    }
+    
+    private static Room createRoom(Integer capacity, String description, Integer price, String number){
+        Room room = new Room();
+        room.setCapacity(capacity);
+        room.setDescription(description);
+        room.setPriceForNight(price);
+        room.setRoomNumber(number);
+        return room;
+    }
+    
+    
 }
